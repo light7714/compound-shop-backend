6feab0d60e9fc01df731536dad9c36734449163e
Shubham Vermani <shubham.vermani14@gmail.com>
2023-07-12 23:52:40 +0530

    add comment
    explaining why .env not used for now

--- config/db.config.js
+++ config/db.config.js
@@ -1,3 +1,4 @@
+// not using .env file as of now, as there was a problem that my code was not picking up the .env filed defined in this app (as I'm working on company laptop)
 module.exports = {
   HOST: "db4free.net",
   USER: "shubham14",
